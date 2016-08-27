3cd6a178c9d533d41e8779cce12c0d137ce46c08
Zerui Hu <me@huzerui.com>
2016-08-27 15:12:56 +0800

    subtle change

--- js/main.js
+++ js/main.js
@@ -8,15 +8,15 @@ window.onload = function() {
     };
     //检测平台 
     var p = navigator.platform;
-    system.win = p.indexOf("Win") == 0;
-    system.mac = p.indexOf("Mac") == 0;
-    system.x11 = (p == "X11") || (p.indexOf("Linux") == 0);
-    system.ipad = (navigator.userAgent.match(/iPad/i) != null) ? true : false;
-    //跳转语句，如果是手机访问就自动跳转到wap.baidu.com页面 
+    system.win = p.indexOf("Win") === 0;
+    system.mac = p.indexOf("Mac") === 0;
+    system.x11 = (p == "X11") || (p.indexOf("Linux") === 0);
+    system.ipad = (navigator.userAgent.match(/iPad/i) !== null) ? true : false;
+    //移动和PC分别跳转不同页面
+    var weibo = document.querySelector("#weibo") || document.getElementById("weibo");
     if (system.win || system.mac || system.xll || system.ipad) {
-        $('.weibo').attr("href", "http://weibo.com/alex1504")
+        weibo.href="http://weibo.com/alex1504";
     } else {
-
-        $('.weibo').attr("href", "http://weibo.cn/alex1504")
+        weibo.href="http://weibo.cn/alex1504";
     }
 };
