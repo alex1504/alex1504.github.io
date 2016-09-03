92d95f2b71e766ca00612346d6ed6c6dc64d469c
Zerui Hu <me@huzerui.com>
2016-09-03 22:52:35 +0800

    remove child

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 window.onload = function() {
-    // 隐藏loading模块，显示主体
-    document.getElementById("m-loading").style['z-index'] = "-9999";
+    // 移除loading模块，显示主体
+    document.body.removeChild(document.getElementById("m-loading"));
     document.getElementById("page").style.display = "block";
     //平台、设备和操作系统 
     var system = {
