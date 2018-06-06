764f6bc0261c9bba183739a88a974b74f61e2a21
alex1504 <alex1504@163.com>
2018-06-06 23:10:30 +0800

    update style and image preload

--- js/main.js
+++ js/main.js
@@ -1,46 +1,71 @@
-(function() {
+(function () {
 
     // 移除loading模块，显示主体
     function preload() {
-        var imgDoms = document.querySelectorAll('img');
-        var imgLen = imgDoms.length;
+        var imgArr = [
+            './images/avatar.jpg',
+            './images/avatar.gif',
+            './images/1.jpg',
+            './images/crab.gif',
+            './images/header-bg.jpg',
+            './images/blog_bg.jpg',
+            './images/jianshu_bg.jpg',
+            './images/github_bg.jpg',
+            './images/weibo_bg.jpg',
+            './images/lofter_bg.jpg'
+        ]
+        var imgLen = imgArr.length;
         var loadedLen = 0;
-        Array.prototype.forEach.call(imgDoms, function(imgDom) {
-            var src = imgDom.getAttribute('src');
-            var img = new Image();
-            img.addEventListener('load', function() {
-                loadedLen++;
-                if (loadedLen == imgLen) {
-                    document.body.removeChild(document.getElementById("m-loading"));
-                    document.getElementById("page").style.display = "block";
-                }
+        Array
+            .prototype
+            .forEach
+            .call(imgArr, function (src) {
+                var img = new Image();
+                img.addEventListener('load', function () {
+                    loadedLen++;
+                    if (loadedLen == imgLen) {
+                        document
+                            .body
+                            .removeChild(document.getElementById("m-loading"));
+                        document
+                            .getElementById("page")
+                            .style
+                            .display = "block";
+                    }
+                })
+                img.addEventListener('error', function () {
+                    loadedLen++;
+                    if (loadedLen == imgLen) {
+                        document
+                            .body
+                            .removeChild(document.getElementById("m-loading"));
+                        document
+                            .getElementById("page")
+                            .style
+                            .display = "block";
+                    }
+                })
+                img.src = src;
             })
-            img.addEventListener('error', function() {
-                loadedLen++;
-                if (loadedLen == imgLen) {
-                    document.body.removeChild(document.getElementById("m-loading"));
-                    document.getElementById("page").style.display = "block";
-                }
-            })
-            img.src = src;
-        })
     }
     preload();
 
     function handleWeiboHref() {
-        //平台、设备和操作系统 
+        //平台、设备和操作系统
         var system = {
             win: false,
             mac: false,
             xll: false,
             ipad: false
         };
-        //检测平台 
+        //检测平台
         var p = navigator.platform;
         system.win = p.indexOf("Win") === 0;
         system.mac = p.indexOf("Mac") === 0;
         system.x11 = (p == "X11") || (p.indexOf("Linux") === 0);
-        system.ipad = (navigator.userAgent.match(/iPad/i) !== null) ? true : false;
+        system.ipad = (navigator.userAgent.match(/iPad/i) !== null)
+            ? true
+            : false;
         var weibo = document.querySelector("#weibo") || document.getElementById("weibo");
         if (system.win || system.mac || system.xll || system.ipad) {
             weibo.href = "http://weibo.com/alex1504";
@@ -55,11 +80,11 @@
         var jpgAvatar = document.querySelector("#avatar");
         var gifAvatar = document.querySelector("#avatar-gif");
 
-        avatarBox.addEventListener('mouseenter', function() {
+        avatarBox.addEventListener('mouseenter', function () {
             jpgAvatar.classList.value = 'z-hide';
             gifAvatar.classList.value = '';
         })
-        avatarBox.addEventListener('mouseleave', function() {
+        avatarBox.addEventListener('mouseleave', function () {
             gifAvatar.classList.value = 'z-hide';
             jpgAvatar.classList.value = '';
         })
